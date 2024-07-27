d370fca7c075ee9a57938724eab9f664610dff98
mmajid5 <mm5764@gmail.com>
2024-07-27 00:37:06 -0400

    initial commit

--- backend/settings.py
+++ backend/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DB_SECRETKEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [os.getenv("DB_LH"), os.getenv("DB_BD"),
                  os.getenv("DB_RAILWAY")]
